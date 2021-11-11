985e0731c153a36860c8ac4a432557701249e3d5
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2021-11-11 19:14:04 +0330

    ViewComponents Added

---
+++ AM4_MVC/Models/About.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AM4_MVC.Models
+{
+    public class About
+    {
+        public long Id { get; set; }
+        public string Name  { get; set; }
+        public string City  { get; set; }
+        public string Description1  { get; set; }
+        public string Description2  { get; set; }
+        public string Experience { get; set; }
+        public string Image { get; set; }
+
+
+        public About(long id, string name, string city, string description1, string description2, string experience, string image)
+        {
+            Id = id;
+            Name = name;
+            City = city;
+            Description1 = description1;
+            Description2 = description2;
+            Experience = experience;
+            Image = image;
+        }
+
+    }
+}
---
+++ AM4_MVC/ViewComponents/AboutViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AM4_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AM4_MVC.ViewComponents
+{
+    public class AboutViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var About = new List<About>()
+            {
+                new About(1,"امیرمهدی","مشهد","امیرمهدی امینی علاقه مند به برنامه نویسی",
+                    "امیدوارم بتونم همکاری خوبی با شما داشته باشم","1   ","1.jpg")
+            };
+            return View("_About",About);
+        }
+    }
+}
